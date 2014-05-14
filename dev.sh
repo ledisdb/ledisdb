5a5081c34e6b93e8063af0d0f8a1808cf0b1f2e8
siddontang <siddontang@gmail.com>
2014-05-14 08:48:31 +0800

    add c++ cgo_flag

--- dev.sh
+++ dev.sh
@@ -27,6 +27,7 @@ function add_path()
 }
 
 export CGO_CFLAGS="-I$LEVELDB_DIR/include -I$SNAPPY_DIR/include"
+export CGO_CXXFLAGS="-I$LEVELDB_DIR/include -I$SNAPPY_DIR/include"
 export CGO_LDFLAGS="-L$LEVELDB_DIR/lib -L$SNAPPY_DIR/lib -lsnappy"
 export LD_LIBRARY_PATH=$(add_path $LD_LIBRARY_PATH $SNAPPY_DIR/lib)
 export LD_LIBRARY_PATH=$(add_path $LD_LIBRARY_PATH $LEVELDB_DIR/lib)
