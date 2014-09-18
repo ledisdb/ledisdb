fafd0c7e807580f9de0437c1f42656b304b2a994
siddontang <siddontang@gmail.com>
2014-09-18 10:25:15 +0800

    add flag for pprof

--- cmd/ledis-server/main.go
+++ cmd/ledis-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/siddontang/ledisdb/config"
 	"github.com/siddontang/ledisdb/server"
 	"log"
@@ -15,6 +16,8 @@ import (
 
 var configFile = flag.String("config", "", "ledisdb config file")
 var dbName = flag.String("db_name", "", "select a db to use, it will overwrite the config's db name")
+var usePprof = flag.Bool("pprof", false, "enable pprof")
+var pprofPort = flag.Int("pprof_port", 6060, "pprof http port")
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -60,9 +63,11 @@ func main() {
 		app.Close()
 	}()
 
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
+	if *usePprof {
+		go func() {
+			log.Println(http.ListenAndServe(fmt.Sprintf(":%d", *pprofPort), nil))
+		}()
+	}
 
 	app.Run()
 }
