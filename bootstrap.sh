abb4e8c90337d016b80a3b48b4a51d5a6680478b
siddontang <siddontang@gmail.com>
2014-11-21 09:13:48 +0800

    update godep bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@
 godep path > /dev/null 2>&1
 if [ "$?" = 0 ]; then
     GOPATH=`godep path`
+    # https://github.com/tools/godep/issues/60
+    # have to rm Godeps/_workspace first, then restore
+    rm -rf $GOPATH
     godep restore
     exit 0
 fi
