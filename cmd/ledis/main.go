a6db04b51d09993661c36f5c210067669fa4308e
Lunny Xiao <xiaolunwen@gmail.com>
2020-05-09 21:12:59 +0800

    Add help

--- cmd/ledis/main.go
+++ cmd/ledis/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/ledisdb/ledisdb/cmd"
 )
+
+var (
+	cmds = [][]string{
+		{"server", "run ledis server"},
+		{"cli", "run ledis client"},
+		{"repair", "repair ledis storage directory"},
+		{"dump", "create a snapshort of ledis"},
+		{"load", "load data from a snapshort"},
+		{"benchmark", "run the benchmarks with ledis"},
+	}
+)
+
+func printSubCmds() {
+	for _, cmd := range cmds {
+		printCmd(cmd[0], cmd[1])
+	}
+}
+
+func printCmd(cmd, description string) {
+	fmt.Printf("%s\t- %s\n", cmd, description)
+}
 
 func main() {
 	var subCmd string
@@ -23,6 +45,8 @@ func main() {
 		cmd.CmdCli()
 	case "dump":
 		cmd.CmdDump()
+	case "help":
+		printSubCmds()
 	case "server":
 		fallthrough
 	default:
